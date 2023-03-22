1d31dfc6b7abb58340fe7746232d276b22a13af0
1605129 <1605129@uab.cat>
2023-03-22 12:29:53 +0100

    Esquema cliente, Bucles básicos

--- src/ttorrent.c
+++ src/ttorrent.c
@@ -43,6 +43,79 @@ int main(int argc, char **argv) {
 	(void) MSG_REQUEST;
 	(void) MSG_RESPONSE_NA;
 	(void) MSG_RESPONSE_OK;
+	
+	//Clase con Chow
+	
+	char downloaded_file[] = "./torrent_samples/client/testfile";
+	char *downloaded_file = "./torrent_samples/client/testfile";
+	
+	create_torrent_from_metainfo_file(argv[1], &tor, downloaded_file);
+	char buffer[MAX_BLOCK_SIZE];
+	//SObre el bucle
+	if(/*Si tenemos todos los bloques usando block_map*/){
+	//Con found y count podemos comprobar esta opción (buscar los bloques)
+	}//En este caso no haremos nada del siguiente código (siguientes lineas)
+	
+	
+	for(int i = 0; i < ) //peer_count{
+		int s;
+		//port en la foto
+		int c = connect(s, &addr, sizeof(addr));
+		/*
+		0 peer_address << 24
+		0 peer_address << 16
+		0 peer_address << 8
+		0 peer_address << 0
+		
+		"127.0.0.1" host byte orde = 0x7F
+		            network byte order = 0x01
+		FOTO
+		0 peer_address << 24 uitn8_t
+		0 peer_address << 16 uitn8_t
+		0 peer_address << 8 uitn8_t
+		0 peer_address << 0 uitn8_t
+		*/
+		if(c == -1)
+		{
+			perror();
+			close(s);
+			continue;
+		}
+	
+	
+	
+	for(int j = 0; j < ) {// block_count
+		//UNa opcion
+		if(tor.block_map[j] == 1){
+			//Tiene info sobre el bloque que tenemos en el disco
+			//Si hemos sacado bloque 1 y no 2 ni 3
+			/*
+				[0] == 1
+				[1] == 0
+				[2] == 0
+			
+			*/
+			continue; //Pasamos a la siguiente interación
+		}
+		//El resto del codigo
+		//otra opcion
+		if(tor.block_map[j] == 0){
+			//Lo hacemos todo
+		}
+	
+	
+	
+	}
+	//Bajo este for
+	close(s);
+	if(/*SI tenemos todos los bloques usando block_map*/){
+		
+	}
+	
+
+	
+	
+	
 
 	return 0;
 }
